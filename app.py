a66005817ed74d38c9097b6f2df825f469a6a8dd
amgonzalezvargas <andresmgv@gmail.com>
2025-03-25 17:19:46 -0500

    v0

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # app.py
 from flask import Flask, render_template, request
 import os
-from utils import get_countries, get_country_items, get_job_areas, search_jobs
 
 
 app = Flask(__name__)
@@ -10,52 +9,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-
-@app.route('/countries')
-def countries():
-    
-    return render_template('countries.html')
-
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
-from flask import request
-from utils import get_countries, get_country_items
-
-@app.route('/search', methods=['GET', 'POST'])
-def search_country():
-    countries = get_countries()
-    selected_country = None
-    items = []
-    if request.method == 'POST':
-        selected_country = request.form.get('country')
-        items = get_country_items(selected_country)
-    return render_template('search_country.html', countries=countries, selected_country=selected_country, items=items)
-
-@app.route('/search_job', methods=['GET', 'POST'])
-def search_job():
-    countries = get_countries()
-    job_areas = get_job_areas()
-    results = []
-    sort_by = request.args.get('sort_by', 'job_title')
-    sort_dir = request.args.get('sort_dir', 'asc')
-    
-    if request.method == 'POST':
-        country = request.form.get('country', '')
-        area = request.form.get('area', '')
-        keywords = request.form.get('keywords', '')
-        
-        results = search_jobs(country, area, keywords, sort_by, sort_dir)
-        
-    return render_template('search_job.html', 
-                          countries=countries, 
-                          job_areas=job_areas, 
-                          results=results,
-                          sort_by=sort_by,
-                          sort_dir=sort_dir)
-
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -8,44 +8,6 @@ from db_config import DB_CONFIG
 import mysql
 import mysql.connector  # This imports the specific connector module
 
-
-
-def get_countries():
-    try:
-        connection = mysql.connector.connect(**DB_CONFIG)
-        cursor = connection.cursor()
-        query = "SELECT country_name FROM country ORDER BY country_name"
-        cursor.execute(query)
-        countries = [row[0] for row in cursor.fetchall()]
-        cursor.close()
-        connection.close()
-        return countries
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return []
-
-def get_country_items(country_name):
-    try:
-        connection = mysql.connector.connect(**DB_CONFIG)
-        cursor = connection.cursor()
-        query = """
-        SELECT i.item_name, i.unit_cost_USD
-        FROM item i
-        JOIN country c ON i.country_id = c.country_id
-        WHERE c.country_name = %s
-        ORDER BY i.item_name
-        """
-        cursor.execute(query, (country_name,))
-        items = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return items
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return []
-
-
-
 def create_html_content(img_data):
     return f'''
     <!DOCTYPE html>
@@ -90,77 +52,5 @@ def create_html_content(img_data):
     </html>
     '''
 
-
-def get_job_areas():
-    try:
-        connection = mysql.connector.connect(**DB_CONFIG)
-        cursor = connection.cursor()
-        query = "SELECT area_name FROM job_area ORDER BY area_name"
-        cursor.execute(query)
-        areas = [row[0] for row in cursor.fetchall()]
-        cursor.close()
-        connection.close()
-        return areas
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return []
-
-def search_jobs(country, area, keywords, sort_by='job_title', sort_dir='asc'):
-    try:
-        connection = mysql.connector.connect(**DB_CONFIG)
-        cursor = connection.cursor()
-        
-        # Construir la consulta base
-        query = """
-        SELECT c.country_name, comp.company_name, ja.area_name, j.job_title, j.month_mean_salary
-        FROM job j
-        JOIN country c ON j.country_id = c.country_id
-        JOIN company comp ON j.company_id = comp.company_id
-        JOIN job_area ja ON j.area_id = ja.area_id
-        WHERE 1=1
-        """
-        
-        params = []
-        
-        # Añadir condiciones según la entrada del usuario
-        if country and country != 'All':
-            query += " AND c.country_name = %s"
-            params.append(country)
-            
-        if area and area != 'All':
-            query += " AND ja.area_name = %s"
-            params.append(area)
-            
-        if keywords:
-            # Dividir palabras clave y crear condición para cada una
-            keyword_list = keywords.split()
-            for keyword in keyword_list:
-                query += " AND j.job_title LIKE %s"
-                params.append(f"%{keyword}%")
-        
-        # Añadir cláusula ORDER BY
-        valid_columns = {
-            'country': 'c.country_name',
-            'company': 'comp.company_name',
-            'area': 'ja.area_name',
-            'job_title': 'j.job_title',
-            'salary': 'j.month_mean_salary'
-        }
-        
-        order_col = valid_columns.get(sort_by, 'j.job_title')
-        order_dir = 'DESC' if sort_dir.lower() == 'desc' else 'ASC'
-        
-        query += f" ORDER BY {order_col} {order_dir}"
-        
-        cursor.execute(query, params)
-        results = cursor.fetchall()
-        
-        cursor.close()
-        connection.close()
-        
-        return results
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return []
 
 
